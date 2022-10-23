d9148a13dddfe157f68b24dbc8049610f0f72e06
Arthur Macena <arthur.macena@ccc.ufcg.edu.br>
2022-10-23 14:01:26 -0300

    pasta base do projeto

---
+++ Calculadora de Salário/src/funcao/Aplicacao.java
@@ -0,0 +1,14 @@
+package funcao;
+
+public class Aplicacao {
+
+    private boolean funciona;
+
+    public Aplicacao(){
+        this.funciona = true;
+    }
+
+    public boolean getFunciona() {
+        return funciona;
+    }
+}
