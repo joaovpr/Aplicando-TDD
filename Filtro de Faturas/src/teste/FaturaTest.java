34cff6068c9feaa4e215cd9888513ab4008503ec
Arthur Macena <arthur.macena@ccc.ufcg.edu.br>
2022-10-24 20:55:19 -0300

    adding attributes validations

--- Filtro de Faturas/src/teste/FaturaTest.java
+++ Filtro de Faturas/src/teste/FaturaTest.java
@@ -11,15 +11,39 @@ class FaturaTest {
     private Fatura faturaAttributesTest;
 
     @BeforeEach
-    void beforeTest(){
+    void beforeTest() {
         this.faturaAttributesTest = new Fatura("123", 20.0, "23/10/2022", "Arthur");
     }
 
     @Test
-    void assertAttributesTest(){
+    void assertAttributesTest() {
         Assertions.assertNotNull(this.faturaAttributesTest.getCodigo());
         Assertions.assertNotNull(this.faturaAttributesTest.getValor());
         Assertions.assertNotNull(this.faturaAttributesTest.getData());
         Assertions.assertNotNull(this.faturaAttributesTest.getCliente());
+
+        // Validações de Atributos
+        try {
+            this.faturaAttributesTest = new Fatura(null, 20.0, "23/10/2022", "Arthur");
+        } catch (Exception e) {
+            Assertions.assertEquals(e.getMessage(), "Variable can't be null");
+        }
+        try {
+            this.faturaAttributesTest = new Fatura("123", -1, "23/10/2022", "Arthur");
+        } catch (Exception e) {
+            Assertions.assertEquals(e.getMessage(), "Valor can't be negative");
+        }
+        try {
+            this.faturaAttributesTest = new Fatura("123", 20.0, null, "Arthur");
+        } catch (Exception e) {
+            Assertions.assertEquals(e.getMessage(), "Variable can't be null");
+        }
+        try {
+            this.faturaAttributesTest = new Fatura("123", 20.0, "23/10/2022", null);
+        } catch (Exception e) {
+            Assertions.assertEquals(e.getMessage(), "Variable can't be null");
+        }
+
+
     }
 }
