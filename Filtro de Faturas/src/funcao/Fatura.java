322f4e55a49540e22f282fc4132a922cd04d4c38
Arthur Macena <arthur.macena@ccc.ufcg.edu.br>
2022-10-24 20:44:10 -0300

    adicionado objeto com os atributos

--- Filtro de Faturas/src/funcao/Fatura.java
+++ Filtro de Faturas/src/funcao/Fatura.java
@@ -1,4 +1,35 @@
 package funcao;
 
 public class Fatura {
+
+    private String codigo;
+
+    private double valor;
+
+    private String data;
+
+    private String cliente;
+
+    public Fatura(String codigo, double valor, String data, String cliente) {
+        this.codigo = codigo;
+        this.valor = valor;
+        this.data = data;
+        this.cliente = cliente;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
 }
--- Filtro de Faturas/src/teste/FaturaTest.java
+++ Filtro de Faturas/src/teste/FaturaTest.java
@@ -1,7 +1,6 @@
 package teste;
 
 import funcao.Fatura;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
